1f200a377204028322b70a0c1242536fd423cfaf
ruud00000 <ruudnaastepad@hotmail.com>
2023-11-29 17:28:16 +0100

    test update

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,38 +1,40 @@
-const request = require('supertest');
-const server = require('../index');
+const request = require('supertest')
+const server = require('../index')
+// testbestand om te uploaden:
+const filename = 'C:/Users/Ruud Naastepad/Downloads/WKB/storyboard/pdf-upload-pc.png'
 
 describe('File Upload API', () => {
-  let server
-  beforeAll(async () => {
-    const mod = await import('..');
-    server = mod.default;
-  });
-
-  afterAll((done) => {
-    if (server) {
-      server.close(done);
-    }
-  });
 
   test('should upload a file', async () => {
     const response = await request(server)
       .post('/upload')
-      .attach('file', 'C:/Users/Ruud Naastepad/Downloads/WKB/storyboard/pdf-upload-pc.png'); // Replace with the path to a test file
+      .attach('file', filename); 
 
-    expect(response.status).toBe(200);
-    expect(response.body.message).toBe('Het bestand is geupload!');
+    expect(response.status).toBe(200)
+    expect(response.body.message).toBe('Het bestand is geupload!')
+  })
+
+  test('should say no files uploaded with status 400', async () => {
+    const response = await request(server)
+      .post('/upload')
+      .attach('file', null); 
+
+    expect(response.status).toBe(400)
+    expect(response.body.message).toBe('Er zijn geen bestanden geupload.')
   })
 
   test('should reset message after 5 seconds', async () => {
     const response = await request(server)
       .post('/upload')
-      .attach('file', 'C:/Users/Ruud Naastepad/Downloads/WKB/storyboard/pdf-upload-pc.png'); // Replace with the path to a test file
+      .attach('file', filename); 
 
       // Wait for 6 seconds to ensure the message is reset
-    await new Promise(resolve => setTimeout(resolve, 6000));
+    await new Promise(resolve => setTimeout(resolve, 6000))
 
     // Check if the message is reset to an empty string
-    const resetResponse = await request(server).get('/');
-    expect(resetResponse.text).toContain('<p id="message"></p>');
-  }, 7000);
-});
+    const resetResponse = await request(server).get('/')
+    expect(resetResponse.text).toContain('<p id="message"></p>')
+  }, 7000)
+
+  server.close()
+})
